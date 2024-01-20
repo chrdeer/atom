572d2a9cee1dfb6bc425a0e7b3f7f1799283e2b2
scopetion <143470966+chrdeer@users.noreply.github.com>
2024-01-20 10:37:39 +0800

    update

--- src/api/reducer.js
+++ src/api/reducer.js
@@ -14,7 +14,7 @@ const reducer = (state, action) => {
         case 'CONNECT_ERROR':
             return { ...state, apiState: 'ERROR', apiError: action.payload };
         case 'SET_ACCOUNT':
-            return { ...state, account: action.payload }
+            return { ...state, account: action.payload };
         case "SET_NETWORKID":
             return { ...state, networkId: action.payload };
         case "SET_BALANCE":
